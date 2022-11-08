e311d6bcc444118d8c5d33787d8a379554ec8baa
Andy Walker <andy.walker@modulrfinance.com>
2022-11-08 13:35:52 +0000

    Adding library and reversing code
    Adding commons-lang3 in Project Structure and saving Run Config and code to print reversed name

--- src/com/andrewwalkerscotland/Main.java
+++ src/com/andrewwalkerscotland/Main.java
@@ -1,7 +1,10 @@
 package com.andrewwalkerscotland;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello ");
+        System.out.print("Hello ");
+        System.out.println(StringUtils.reverse(args[0]));
     }
 }
